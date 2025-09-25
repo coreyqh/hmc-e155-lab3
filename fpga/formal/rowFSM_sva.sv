17fecb0579a5ba0a81bfb549cd957c3778d191f5
Corey Hickson <chickson@hmc.edu>
2025-09-25 03:46:22 -0700

    rowFSM SVA active high

--- fpga/formal/rowFSM_sva.sv
+++ fpga/formal/rowFSM_sva.sv
@@ -7,11 +7,11 @@ always @* assume (~rstn == $initstate);
 always @(posedge clk) begin
     if (rstn && $past(rstn)) begin
         assert (row != $past(row));
-        assert ($onehot(~row));
+        assert ($onehot(row));
 
-        assert (eventually(row == 4'b0111));
-        assert (eventually(row == 4'b1011));
-        assert (eventually(row == 4'b1101));
-        assert (eventually(row == 4'b1110));
+        assert (eventually(row == 4'b1000));
+        assert (eventually(row == 4'b0100));
+        assert (eventually(row == 4'b0010));
+        assert (eventually(row == 4'b0001));
     end
 end
