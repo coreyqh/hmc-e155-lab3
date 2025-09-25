10252d38151523d71e4588fd7e3d42598d2673d2
Corey Hickson <chickson@hmc.edu>
2025-09-25 03:57:19 -0700

    debouncer SVA active high

--- fpga/formal/debouncer_sva.sv
+++ fpga/formal/debouncer_sva.sv
@@ -6,10 +6,10 @@ always @* assume (~rstn == $initstate);
 always @(posedge clk) if ($past(high) || $past(low)) assume (!req);
 always @(posedge clk) if ($past(req)) assume (!$changed(activeCol));
 always @(posedge clk) if ($past(req)) assume (!$changed(activeRow));
-always @* assume ($onehot(~activeCol) || activeCol == 4'b1111);
-always @* if (req) assume ($onehot(~activeCol));
-always @* assume ($onehot(~activeRow));
-always @* assume ($onehot(~row));
+always @* assume ($onehot(activeCol) || activeCol == 4'b0000);
+always @* if (req) assume ($onehot(activeCol));
+always @* assume ($onehot(activeRow));
+always @* assume ($onehot(row));
 always @(posedge clk) assume (row == {$past(row[0]), $past(row[3:1])});
 
 always @(posedge clk) begin
@@ -17,17 +17,15 @@ always @(posedge clk) begin
         assert (state != ERROR);
         assert(~(low && high));
         if (!req && !$past(req)) assert (~(low || high));
-        // if ($past(row, 2) != $past(activeRow, 2)) begin
-        //     assert (count == $past(count));
-        //     assert (state == $past(state));
-        // end
         if (state != $past(state))
             assert (count == 0);
         if (count == THRESHOLD && state == LOW)
             assert (low);
         if (count == THRESHOLD && state == HIGH)
             assert (high);
-        // if (req && row == activeRow && ((state == high && ((~activeCol & col) != 4'b0)) || (state == low && ((~activeCol & col) == 4'b0))))
-        //     assert (nextcount == count + 1);
+        if (req && ((col & activeCol) != 0) &&  row == activeRow)
+            assert (nextstate == HIGH);
+        if (req && ((col & activeCol) == 0) &&  row == activeRow)
+            assert (nextstate == LOW);
     end
 end
